2cd3cd3f1787459f76d9691fb07737ccdc4bf0e6
Juan Vega <spacecarlos0686@gmail.com>
2021-10-01 22:49:11 -0500

    end of section4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
@@ -15,9 +16,9 @@ namespace API.Controllers
        
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities()
+        public async Task<ActionResult<List<Activity>>> GetActivities(CancellationToken ct)
         {
-            return await Mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query(), ct);
             
         }
 
@@ -38,6 +39,12 @@ namespace API.Controllers
         public async Task<IActionResult> EditActivity(Guid id, Activity activity){
             activity.Id = id;
             return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id){
+            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
+
         }
     }
 }
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
@@ -16,12 +18,27 @@ namespace Application.Activities
         {
 
             private readonly DataContext _context;
-            public Handler(DataContext context){
+            private readonly ILogger<List> _logger;
+
+            public Handler(DataContext context, ILogger<List> logger){
                 _context = context;
+                _logger = logger;
             }
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken){
 
-                return await _context.Activities.ToListAsync();
+
+                // try{
+                //     for(var i = 0; i < 10; i++){
+                //         cancellationToken.ThrowIfCancellationRequested();
+                //         await Task.Delay(1000, cancellationToken);
+                //         _logger.LogInformation($"Task {i} has completed");
+                //     }
+
+                // }catch(Exception ex)when (ex is TaskCanceledException ) {
+                //     _logger.LogInformation($"Task was cancelled");
+                // }
+
+                return await _context.Activities.ToListAsync(cancellationToken);
             } 
         }
         
